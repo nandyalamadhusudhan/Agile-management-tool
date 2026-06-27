9691e59dd42ebfbe86f3315555398a4ee58f575f
nandyalamadhusudhan <nandyalamadhusudhan113@gmail.com>
2026-06-27 16:26:28 +0530

    updated homepage again

--- frontend/src/components/homepage.jsx
+++ frontend/src/components/homepage.jsx
@@ -4,12 +4,12 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 function Home() {
   const navigate = useNavigate();
- /* useEffect(() => {
+  useEffect(() => {
   const token = localStorage.getItem("token");
   if (token) {
     navigate("/mainpage", { replace: true });
   }
-}, [navigate]);*/
+}, [navigate]);
   return (
     <div className="container">
       {/* Navigation Bar */}
